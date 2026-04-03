b724594d6a6cb4c54b242e033f4aa7878c347845
pcb2002 <machat350@gmail.com>
2026-04-03 11:41:33 +0900

    Solution 1

--- src/Lv2/Solution_1.java
+++ src/Lv2/Solution_1.java
@@ -8,7 +8,42 @@
 //n	return
 //        3	"수박수"
 //        4	"수박수박"
+
+// 처음 썼던 로직
+//public class Solution {
+//    public static void main(String[] args) {
+//        int n = 4;
+//        for (int i = 0; i < n; i++) {
+//            System.out.print(solution(i));
+//        }
+//    }
+//
+//    public static String solution(int n) {
+//        String answer = "";
+//        if (n%2 == 0) answer = "수";
+//        else if (n%2 == 1) answer = "박";
+//
+//        return answer;
+//    }
+//}
+
+// 틀린 이유: Solution 안에 완성된 한 문장(수박 4개가 한박스에)이 출력되기를 기대하기 때문에
+// 출력 결과 자체는 같지만 수박 4개를 개별포장해서 제출한 것과 같음.
+
 package Lv2;
 
 public class Solution_1 {
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.print(solution(n));
+    }
+
+    public static String solution(int n) {
+        String answer = "";
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) answer += "수";
+            else answer += "박";
+        }
+        return answer;
+    }
 }
